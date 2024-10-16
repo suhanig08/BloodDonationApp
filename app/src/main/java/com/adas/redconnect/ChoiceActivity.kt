6f9507b004c5533b256d35a8ca08ea49c426fc0a
Suhani Gupta <pubgsuhanig@gmail.com>
2024-10-16 12:28:05 +0530

    s

--- app/src/main/java/com/adas/redconnect/ChoiceActivity.kt
+++ app/src/main/java/com/adas/redconnect/ChoiceActivity.kt
@@ -1,20 +1,37 @@
 package com.adas.redconnect
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.adas.redconnect.databinding.ActivityChoiceBinding
+import com.adas.redconnect.databinding.ActivityDonorAuthBinding
 
 class ChoiceActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityChoiceBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityChoiceBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_choice)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.donorBtn.setOnClickListener {
+            val i= Intent(this,DonorAuth::class.java)
+            startActivity(i)
+        }
+
+        binding.hospitalBtn.setOnClickListener {
+            val i= Intent(this,HospitalAuth::class.java)
+            startActivity(i)
         }
     }
 }
